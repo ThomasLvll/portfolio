8aad7f896f0444e601bf7426d60d186bd2a5844d
ThomasLvll <leveille.thomas77@gmail.com>
2021-06-04 21:11:23 +0200

    responsive & dark theme improvements

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ if (isset($_GET["lang"]))
         <!-- external CSS goes below -->
         <link rel="stylesheet" type="text/css" href="/res/css/index.css?v=<?= time() ?>" />
         <link rel="stylesheet" type="text/css" href="/res/css/index.dark.css?v=<?= time() ?>" media="(prefers-color-scheme: dark)" />
+        <link rel="stylesheet" type="text/css" href="/res/css/index.1024px.css?v=<?= time() ?>" media="(max-width: 1024px)" />
     </head>
     <body>
         <div id="topbar"><div id="topbar-nav-menu">
@@ -26,11 +27,15 @@ if (isset($_GET["lang"]))
             <span id="header-title">Développeur Fullstack</span>
             <div id="front-wave"><?= file_get_contents("./res/img/front-wave.svg") ?></div>
     </div>
-        <div id="content-flow">Bonjour !
+        <div id="content-flow">
+            <div id="about-me" class="anchor">
+                <div id="profile-picture"></div>
+                <div id="profile-description"><span class="title">Thomas Léveillé, 20 ans</span class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit diam risus sit justo, consectetur vehicula consequat volutpat. Consequat egestas nisi, pharetra proin nisl vel <a target="_blank" href="https://www.ecole-ipssi.com">IPSSI Paris</a>. Laoreet placerat quis in quis feugiat eu porta integer. Quisque eget dictum elementum consectetur gravida. Praesent. Texte de remplissage pour voir la taille du cadre.<span></span></div>
+            </div>
         </div>
 
         <!-- external JS goes below -->
-        <script type="text/javascript" src="/src/js/wavery/wavery.min.js"></script><!-- https://github.com/up2pixy/wavery -->
+        <!-- <script type="text/javascript" src="/src/js/wavery/wavery.min.js"></script> https://github.com/up2pixy/wavery -->
         <script type="text/javascript" src="/res/js/index.js?v=<?= time() ?>"></script>
     </body>
 </html>
