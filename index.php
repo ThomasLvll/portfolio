703e0bec773d4fb8bfacd51688dcc42668724f1b
ThomasLvll <leveille.thomas77@gmail.com>
2021-06-04 22:02:01 +0200

    responsive design improvements

--- index.php
+++ index.php
@@ -26,11 +26,15 @@ if (isset($_GET["lang"]))
             <div id="back-wave"><?= file_get_contents("./res/img/back-wave.svg") ?></div>
             <span id="header-title">Développeur Fullstack</span>
             <div id="front-wave"><?= file_get_contents("./res/img/front-wave.svg") ?></div>
-    </div>
+        </div>
         <div id="content-flow">
-            <div id="about-me" class="anchor">
+            <div id="about-me">
+                <div class="anchor">Présentation</div>
                 <div id="profile-picture"></div>
                 <div id="profile-description"><span class="title">Thomas Léveillé, 20 ans</span class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit diam risus sit justo, consectetur vehicula consequat volutpat. Consequat egestas nisi, pharetra proin nisl vel <a target="_blank" href="https://www.ecole-ipssi.com">IPSSI Paris</a>. Laoreet placerat quis in quis feugiat eu porta integer. Quisque eget dictum elementum consectetur gravida. Praesent. Texte de remplissage pour voir la taille du cadre.<span></span></div>
+            </div>
+            <div id="skills">
+                <div class="anchor">Compétences</div>
             </div>
         </div>
 
