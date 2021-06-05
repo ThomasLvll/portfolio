94be6f4171a2900b1efaf10beeaaefa81557e2ca
ThomasLvll <leveille.thomas77@gmail.com>
2021-06-05 17:42:13 +0200

    major update

--- index.php
+++ index.php
@@ -4,13 +4,19 @@ $lang_code = "fr-FR";
 if (isset($_GET["lang"]))
     $lang_code = $_GET["lang"];
 
+$lang_file_path = "./res/data/locale/$lang_code.json";
+$lang_file_content = file_get_contents($lang_file_path);
+$LANG = json_decode($lang_file_content);
+
+$DATA = json_decode(file_get_contents("./res/data/data.json"));
+
 ?>
 
 <!DOCTYPE html>
 <html lang="fr-FR">
     <head>
         <meta charset="utf-8" />
-        <title>Mon portfolio — Thomas Léveillé</title>
+        <title><?= $LANG->general->page_title ?></title>
         
         <!-- external CSS goes below -->
         <link rel="stylesheet" type="text/css" href="/res/css/index.css?v=<?= time() ?>" />
@@ -19,22 +25,69 @@ if (isset($_GET["lang"]))
     </head>
     <body>
         <div id="topbar"><div id="topbar-nav-menu">
-            <a href="#about-me"><span id="about-me-nav-item" class="active">Présentation</span></a><a href="#skills"><span id="skills-nav-item">Compétences</span></a><a href="#projects"><span id="projects-nav-item">Projets</span></a><a href="#experience"><span id="experience-nav-item">Expérience</span></a><a href="#contact"><span id="contact-nav-item">Contact</span></a>
+            <a href="#about-me"><span id="about-me-nav-item" class="active"><?=
+                $LANG->sections->about_me->nav_menu
+            ?></span></a><a href="#skills"><span id="skills-nav-item"><?=
+                $LANG->sections->skills->nav_menu
+            ?></span></a><a href="#projects"><span id="projects-nav-item"><?=
+                $LANG->sections->projects->nav_menu
+            ?></span></a><a href="#experience"><span id="experience-nav-item"><?=
+                $LANG->sections->experience->nav_menu
+            ?></span></a><a href="#contact"><span id="contact-nav-item"><?=
+                $LANG->sections->contact->nav_menu
+            ?></span></a>
         </div></div>
         <div id="header">
             <div id="header-back"></div>
             <div id="back-wave"><?= file_get_contents("./res/img/back-wave.svg") ?></div>
-            <span id="header-title">Développeur Fullstack</span>
+            <span id="header-title"><?= $LANG->general->header_title ?></span>
             <div id="front-wave"><?= file_get_contents("./res/img/front-wave.svg") ?></div>
         </div>
         <div id="content-flow">
             <div id="about-me">
-                <div class="anchor">Présentation</div>
+                <div class="anchor"><?= $LANG->sections->about_me->anchor ?></div>
                 <div id="profile-picture"></div>
-                <div id="profile-description"><span class="title">Thomas Léveillé, 20 ans</span class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit diam risus sit justo, consectetur vehicula consequat volutpat. Consequat egestas nisi, pharetra proin nisl vel <a target="_blank" href="https://www.ecole-ipssi.com">IPSSI Paris</a>. Laoreet placerat quis in quis feugiat eu porta integer. Quisque eget dictum elementum consectetur gravida. Praesent. Texte de remplissage pour voir la taille du cadre.<span></span></div>
+                <div id="profile-description"><span class="title"><?=
+                        $LANG->sections->about_me->profile_title
+                    ?></span><span class="content"><?=
+                        $LANG->sections->about_me->profile_description
+                    ?></span><div id="social-links"><?php
+                    foreach ($DATA->social_links as $k => $v) {
+                    ?>
+                        <a target="_blank" href="<?=
+                            $DATA->social_links->$k->url
+                        ?>"><span class="icon" style="background-image: url('<?=
+                            $DATA->social_links->$k->icon
+                        ?>');" tooltip="<?=
+                            $LANG->social_links->$k->tooltip
+                        ?>"></span></a>
+                    <?php
+                    }
+                    ?>
+                    </div>
+                </div>
             </div>
             <div id="skills">
-                <div class="anchor">Compétences</div>
+                <div class="anchor"><?= $LANG->sections->skills->anchor ?></div>
+                <?php foreach ($DATA->skills as $skill_row) {
+                ?>
+                    <div class="skill-row">
+                    <?php foreach ($skill_row as $k => $v) {
+                    ?>
+                        <div class="skill"><div class="icon" style="background-image: url('<?=
+                            $v->icon
+                        ?>');"></div><span class="title"><?=
+                            $LANG->skills->$k->title
+                        ?></span><span class="content"><?=
+                            $LANG->skills->$k->description
+                        ?></span></div>
+                    <?php
+                    }
+                    ?>
+                    </div>
+                <?php
+                }
+                ?>
             </div>
         </div>
 
--- res/js/index.js
+++ res/js/index.js
@@ -1,3 +1,35 @@
+function getPosition(element) {
+    const rect = element.getBoundingClientRect();
+    return {
+        left: rect.left + window.scrollX,
+        top: rect.top + window.scrollY
+    };
+}
+
+
 document.body.onload = function() {
-    
+    let tooltip_index = 0;
+
+    const element_list = document.querySelectorAll("[tooltip]");
+    element_list.forEach(function(element) {
+        let tooltip = document.createElement("span");
+        tooltip.classList.add("tooltip");
+        tooltip.id = "tooltip-" + tooltip_index;
+        tooltip.innerText = element.getAttribute("tooltip");
+        document.body.appendChild(tooltip);
+        const pos = getPosition(element);
+        const x = parseInt(pos.left + (element.clientWidth / 2) - (tooltip.clientWidth / 2));
+        const y = parseInt(pos.top + element.clientHeight + 10);
+        tooltip.style.left = x + "px";
+        tooltip.style.top = y + "px";
+        element.setAttribute("tooltip-id", tooltip_index);
+        element.setAttribute("tooltip", "");
+        element.addEventListener("mouseenter", function() {
+            document.querySelector("#tooltip-" + this.getAttribute("tooltip-id")).classList.add("visible");
+        });
+        element.addEventListener("mouseleave", function() {
+            document.querySelector("#tooltip-" + this.getAttribute("tooltip-id")).classList.remove("visible");
+        });
+        tooltip_index ++;
+    });
 };
